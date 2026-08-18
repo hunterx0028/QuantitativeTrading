0860a329d632c047065ad9fc8e5ad4140e3161cf
Leo <leegueishen@gmail.com>
2026-08-18 16:15:45 +0800

    docs：將 trend_one_k_stick_specify_day_volume.py 調整為初始參數

--- Z_ORB_ONE/daily_report/trend_one_k_stick_specify_day_volume.py
+++ Z_ORB_ONE/daily_report/trend_one_k_stick_specify_day_volume.py
@@ -32,7 +32,7 @@ if PROJECT_ROOT not in sys.path:
 from Z_ORB_ONE.stock_data import market_previous_close_indices
 
 CONFIG_PATH = os.path.join(BASE_DIR, "config.ini")
-SPECIFIED_DATE = "20260805"  # 指定要繪圖的日期，格式 YYYYMMDD；空值時使用今天日期
+SPECIFIED_DATE = ""  # 指定要繪圖的日期，格式 YYYYMMDD；空值時使用今天日期
 DISPLAY_INDEX_CODES = ("IX0001", "IX0043")  # 依序以分頁顯示上市、上櫃指數
 DISPLAY_STOCK_CODES = [""]  # 要額外顯示的 4 位數股票代碼；空字串會被忽略，等同空陣列
 
